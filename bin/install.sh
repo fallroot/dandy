a3f433e30dfc49f2f0d4c47f8cf32d07b4209e98
CK Moon <fallroot@gmail.com>
2013-12-11 13:44:18 +0900

    배포 테스트용 개발 브랜치 경로 설정

--- bin/install.sh
+++ bin/install.sh
@@ -6,25 +6,25 @@
 # https://github.com/fallroot/dandy
 #
 # 설치
-# curl https://raw.github.com/fallroot/dandy/master/bin/install.sh | sh
+# curl https://raw.github.com/fallroot/dandy/develop/bin/install.sh | sh
 #
 # 삭제
-# curl https://raw.github.com/fallroot/dandy/master/bin/uninstall.sh | sh
+# curl https://raw.github.com/fallroot/dandy/develop/bin/uninstall.sh | sh
 
-echo "한국어 맞춤법/문법 검사기 도우 “단디”를 설치합니다."
+echo "한국어 맞춤법/문법 검사기 도우미 “단디”를 설치합니다."
 
 echo "홈 디렉토리 아래 .dandy 디렉토리를 만듭니다."
 mkdir -p ~/.dandy
 
 echo "실행 파일을 내려받습니다."
-for file in default.css default.js error.png passed.png run.rb template.html; do
-    curl https://raw.github.com/fallroot/dandy/master/src/$file -o ~/.dandy/$file
+for file in dandy.css dandy.js config.png run.rb finish.rb template; do
+    curl https://raw.github.com/fallroot/dandy/develop/src/$file -o ~/.dandy/$file
 done
 
 echo "워크플로우 파일을 설치합니다."
 
 # 압축한 워크플로우 파일을 해제하여 서비스 디렉토리에 옮긴다.
-curl https://raw.github.com/fallroot/dandy/master/build/dandy.zip -o ~/.dandy/dandy.zip
+curl https://raw.github.com/fallroot/dandy/develop/build/dandy.zip -o ~/.dandy/dandy.zip
 rm -f ~/Library/Services/._dandy.workflow
 unzip ~/.dandy/dandy.zip -d ~/Library/Services
 rm -f ~/.dandy/dandy.zip
