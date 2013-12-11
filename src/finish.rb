313288c3a98c5b1001ee9f96ace549609c8ee744
CK Moon <fallroot@gmail.com>
2013-12-11 16:39:00 +0900

    설정 변경사항이 있을 때만 파일 저장

--- src/finish.rb
+++ src/finish.rb
@@ -8,9 +8,12 @@ result_file = File.join home_dir, 'result'
 result = JSON.parse File.read result_file
 
 # 환경 설정 파일
-config_file = File.join home_dir, 'config'
-File.open(config_file, 'w') do |file|
-    file.write result['config'].to_json
+if result.key? 'config'
+    config_file = File.join home_dir, 'config'
+
+    File.open(config_file, 'w') do |file|
+        file.write result['config'].to_json
+    end
 end
 
 # 선택 문장 가져오기
